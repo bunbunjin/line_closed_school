8a97f725dae02d64e88e253fb619b897ff4c163d
bunbunjin <bunbunjin.aisu@gmail.com>
2020-09-06 18:12:50 +0900

    scheduleを使って直した

--- line_notification.py
+++ line_notification.py
@@ -1,7 +1,7 @@
 import requests
 import time
+import schedule
 from okayama_weather import wrn_okayama
-from datetime import datetime
 
 
 def line_massage(line_massage_notification):
@@ -12,38 +12,45 @@ def line_massage(line_massage_notification):
     requests.post(line_api, headers=headers, data=data)
 
 
+def reserve_mimasaka():
+    wrn = wrn_okayama()
+    if [] == wrn["美作"]:
+        pass
+    else:
+        line_massage(f'現在、美作市に{wrn["美作"]}警報が発令されています。\n'
+                     f'あと10分で決まるぞ！！')
+        time.sleep(60)
+
+
+def decision_mimasaka():
+    wrn = wrn_okayama()
+    if [] == wrn["美作"]:
+        pass
+    else:
+        line_massage(f'美作市に{wrn["美作"]}警報が発令！！\n学校休み')
+        time.sleep(60)
+
+
+def reserve_okayama():
+    wrn = wrn_okayama()
+    if [] == wrn["岡山"]:
+        pass
+    else:
+        line_massage(f'現在、岡山市に{wrn["岡山"]}警報が発令されています。\n10分後に期待だ！！！！')
+
+
+def decision_okayama():
+    wrn = wrn_okayama()
+    if [] == wrn["岡山"]:
+        pass
+    else:
+        line_massage(f'岡山市に{wrn["岡山"]}警報が出ています')
+        time.sleep(60)
+
+
+schedule.every().day.at('05:50').do(reserve_mimasaka)
+schedule.every().day.at('06:00').do(decision_mimasaka)
+schedule.every().day.at('06:50').do(reserve_okayama)
+
 while True:
-    tim = str(datetime.now())
-    now = tim[11:-10]
-
-    if '05:50' == now:
-        wrn = wrn_okayama()
-        if [] == wrn["美作"]:
-            pass
-        else:
-            line_massage(f'現在、美作市に{wrn["美作"]}警報が発令されています。\n'
-                         f'あと10分で決まるぞ！！')
-            time.sleep(60)
-
-    if '06:00' == now:
-        wrn = wrn_okayama()
-        if [] == wrn["美作"]:
-            pass
-        else:
-            line_massage(f'美作市に{wrn["美作"]}警報が発令！！\n学校休み')
-
-    if '06:50' == now:
-        wrn = wrn_okayama()
-        if [] == wrn["岡山"]:
-            pass
-        else:
-            line_massage(f'現在、岡山市に{wrn["岡山"]}警報が発令されています。\n10分後に期待だ！！！！')
-            time.sleep(60)
-
-    if '07:00' == now:
-        wrn = wrn_okayama()
-        if [] == wrn["岡山"]:
-            pass
-        else:
-            line_massage(f'岡山市に{wrn["岡山"]}警報が出ています')
-            time.sleep(60)
+    schedule.run_pending()
